8254a8c2a2e8e8b852081aed5b8ba59d109d6448
Fonzi Vazquez <fonzi.vazquez@fmgsuite.com>
2022-08-19 14:22:56 -0500

    Added Async local and bumped to .NETFramework 4.6.1

--- Allure.Net.Commons/Storage/AllureStorage.cs
+++ Allure.Net.Commons/Storage/AllureStorage.cs
@@ -6,29 +6,31 @@ namespace Allure.Net.Commons.Storage
 {
     internal class AllureStorage
     {
-        private readonly ThreadLocal<LinkedList<string>> stepContext = new ThreadLocal<LinkedList<string>>(() =>
-        {
-            return new LinkedList<string>();
-        });
-
         private readonly ConcurrentDictionary<string, object> storage = new ConcurrentDictionary<string, object>();
+
+        private readonly AsyncLocal<LinkedList<string>> stepContext = new AsyncLocal<LinkedList<string>>();
+
+        public AllureStorage()
+        {
+            stepContext.Value = new LinkedList<string>();
+        }
 
         private LinkedList<string> Steps => stepContext.Value;
 
         public T Get<T>(string uuid)
         {
-            return (T) storage[uuid];
+            return (T)storage[uuid];
         }
 
         public T Put<T>(string uuid, T item)
         {
-            return (T) storage.GetOrAdd(uuid, item);
+            return (T)storage.GetOrAdd(uuid, item);
         }
 
         public T Remove<T>(string uuid)
         {
             storage.TryRemove(uuid, out var value);
-            return (T) value;
+            return (T)value;
         }
 
         public void ClearStepContext()
